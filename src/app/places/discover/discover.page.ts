d758b23b145a4104b839a32a37e95eaee7590bf8
Ali Khorsand <ali.khorsand@live.com>
2020-03-16 20:58:08 +0330

    Initial Commit

---
+++ src/app/places/discover/discover.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
+
+@Component({
+  selector: 'app-discover',
+  templateUrl: './discover.page.html',
+  styleUrls: ['./discover.page.scss'],
+})
+export class DiscoverPage implements OnInit {
+
+  loadedPlaces: Place[];
+  constructor(
+    private placesSv: PlacesService
+  ) { }
+
+  ngOnInit() {
+    this.loadedPlaces = this.placesSv.places;
+  }
+
+}
